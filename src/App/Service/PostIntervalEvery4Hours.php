99a3d63872d1552456e9c4226bb3914c0b8dddf5
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2024-07-17 21:53:01 -0400

    change auto x posts to 4 hour interval

--- src/App/Service/PostIntervalEvery4Hours.php
+++ src/App/Service/PostIntervalEvery4Hours.php
@@ -6,14 +6,39 @@ use Carbon\Carbon;
 class PostIntervalEvery4Hours
 {
     private array $postHours = [
-        0, 4, 8, 12, 16, 20,
+        0 => 4,
+        1 => 4,
+        2 => 4,
+        3 => 4,
+        4 => 8,
+        5 => 8,
+        6 => 8,
+        7 => 8,
+        8 => 12,
+        9 => 12,
+        10 => 12,
+        11 => 12,
+        12 => 16,
+        13 => 16,
+        14 => 16,
+        15 => 16,
+        16 => 20,
+        17 => 20,
+        18 => 20,
+        19 => 20,
+        20 => 24,
+        21 => 24,
+        22 => 24,
+        23 => 24,
     ];
 
-    public static function skipIntervalsFromNow(int $skipping): Carbon
+    public function skipIntervalsFromNow(int $skipRounds): Carbon
     {
-        $now = now();
-        var_dump($now->hour);
+        $now = Carbon::now()->subHours(4);
+        $nextSlot = $this->postHours[$now->hour];
+        $nextSlot = $now->addHours($nextSlot - $now->hour);
+        $nextSlot->addHours($skipRounds * 4);
 
-        return $now;
+        return $nextSlot;
     }
 }
