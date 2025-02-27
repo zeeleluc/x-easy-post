bddb0eca45dbf07cfbb0029d8eb33bcc47a7a38c
Luc Zeelenberg <lucaszeelenberg@gmail.com>
2025-02-27 10:36:37 -0400

    no message

--- src/App/Service/PostIntervalEvery4Hours.php
+++ src/App/Service/PostIntervalEvery4Hours.php
@@ -5,39 +5,39 @@ use Carbon\Carbon;
 
 class PostIntervalEvery4Hours
 {
-    private array $postHours = [
-        0 => 6,
-        1 => 6,
-        2 => 6,
-        3 => 6,
-        4 => 6,
-        5 => 6,
-        6 => 12,
-        7 => 12,
-        8 => 12,
-        9 => 12,
-        10 => 12,
-        11 => 12,
-        12 => 18,
-        13 => 18,
-        14 => 18,
-        15 => 18,
-        16 => 18,
-        17 => 18,
-        18 => 24,
-        19 => 24,
-        20 => 24,
-        21 => 24,
-        22 => 24,
-        23 => 24,
-    ];
+//    private array $postHours = [
+//        0 => 6,
+//        1 => 6,
+//        2 => 6,
+//        3 => 6,
+//        4 => 6,
+//        5 => 6,
+//        6 => 12,
+//        7 => 12,
+//        8 => 12,
+//        9 => 12,
+//        10 => 12,
+//        11 => 12,
+//        12 => 18,
+//        13 => 18,
+//        14 => 18,
+//        15 => 18,
+//        16 => 18,
+//        17 => 18,
+//        18 => 24,
+//        19 => 24,
+//        20 => 24,
+//        21 => 24,
+//        22 => 24,
+//        23 => 24,
+//    ];
 
     public function skipIntervalsFromNow(int $skipRounds): Carbon
     {
-        $now = Carbon::now()->subHours(4);
-        $nextSlot = $this->postHours[$now->hour];
+        $now = Carbon::now();
+        $nextSlot = 15;
         $nextSlot = $now->addHours($nextSlot - $now->hour);
-        $nextSlot->addHours($skipRounds * 6);
+        $nextSlot->addDay($skipRounds);
 
         return $nextSlot;
     }
